c564f3e29922b7585261b1affb8d87bffb44cbbe
henriquerogamer-cell <henriquerogamer@gmail.com>
2026-08-26 10:27:14 -0300

    feat: add PS5 remote play source probe interface

---
+++ remote_ps5_source.h
@@ -0,0 +1,17 @@
+#ifndef ASTRO_REMOTE_PS5_SOURCE_H
+#define ASTRO_REMOTE_PS5_SOURCE_H
+
+#include <time.h>
+
+typedef struct astro_remote_ps5_source_probe {
+  int init_rc;
+  int reg_rc;
+  int remoteplay_initialized;
+  int remoteplay_enabled;
+  int tcp_9295_open;
+  time_t probed_at;
+} astro_remote_ps5_source_probe_t;
+
+int astro_remote_ps5_source_probe(astro_remote_ps5_source_probe_t *out);
+
+#endif
