868435a5b3627f1ba19478dc4e728fd76911785c
henriquerogamer-cell <henriquerogamer@gmail.com>
2026-08-26 14:49:26 -0300

    feat: expose Remote Play enable action

--- remote_service.h
+++ remote_service.h
@@ -34,6 +34,7 @@ void astro_remote_service_init(void);
 int astro_remote_service_ensure_worker(void);
 int astro_remote_service_start(void);
 int astro_remote_service_stop(void);
+int astro_remote_service_enable_remoteplay(void);
 int astro_remote_service_shutdown_worker(void);
 void astro_remote_service_snapshot(astro_remote_state_t *out);
 
