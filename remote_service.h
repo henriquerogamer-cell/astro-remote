62c0b7852ba65aa76bf6172352bcbb77055010de
henriquerogamer-cell <henriquerogamer@gmail.com>
2026-08-26 12:31:56 -0300

    feat: track isolated remote worker state

--- remote_service.h
+++ remote_service.h
@@ -13,15 +13,25 @@ typedef struct astro_remote_state {
   int remoteplay_enabled;
   int remoteplay_tcp_9295;
   int source_probe_rc;
+  int worker_pid;
+  int worker_ppid;
+  int worker_port;
+  int worker_port_open;
+  int worker_stuck;
+  int worker_stat;
+  int worker_start_errno;
+  int worker_exit_status;
   unsigned long generation;
   time_t session_started_at;
   time_t last_change_at;
   time_t source_probed_at;
   char phase[48];
+  char worker_wmesg[16];
+  char worker_lockname[16];
 } astro_remote_state_t;
 
 void astro_remote_service_init(void);
-int astro_remote_service_start(void);
+int astro_remote_service_start(int inherited_server_fd,int inherited_client_fd);
 int astro_remote_service_stop(void);
 void astro_remote_service_snapshot(astro_remote_state_t *out);
 
