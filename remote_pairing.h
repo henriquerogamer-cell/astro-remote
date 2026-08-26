9203f1238e726729b4ba9cfa9a22746ab8c01768
henriquerogamer-cell <henriquerogamer@gmail.com>
2026-08-26 18:03:10 -0300

    feat: switch pairing state to manual PIN flow

--- remote_pairing.h
+++ remote_pairing.h
@@ -15,6 +15,8 @@ typedef struct astro_remote_pairing_state {
     time_t expires_at;
 } astro_remote_pairing_state_t;
 
-int astro_remote_pairing_prepare(astro_remote_pairing_state_t *out);
+/* Manual one-time pairing flow. The PIN is entered by the user from
+ * Settings > System > Remote Play > Link Device on the PS5. */
+int astro_remote_pairing_submit_pin(uint32_t pin, astro_remote_pairing_state_t *out);
 
 #endif
