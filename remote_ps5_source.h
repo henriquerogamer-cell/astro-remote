0be3b799b3d3ce5e946d20b7666add50858824b2
henriquerogamer-cell <henriquerogamer@gmail.com>
2026-08-26 15:14:30 -0300

    feat: add Remote Play protocol preflight state

--- remote_ps5_source.h
+++ remote_ps5_source.h
@@ -2,6 +2,7 @@
 #define ASTRO_REMOTE_PS5_SOURCE_H
 
 #include <time.h>
+#include <stdint.h>
 
 typedef struct astro_remote_ps5_source_probe {
   int init_rc;
@@ -12,7 +13,17 @@ typedef struct astro_remote_ps5_source_probe {
   time_t probed_at;
 } astro_remote_ps5_source_probe_t;
 
+typedef struct astro_remote_ps5_protocol_probe {
+  int rc;
+  int http_status;
+  int nonce_present;
+  uint32_t application_reason;
+  char rp_version[16];
+  time_t probed_at;
+} astro_remote_ps5_protocol_probe_t;
+
 int astro_remote_ps5_source_probe(astro_remote_ps5_source_probe_t *out);
 int astro_remote_ps5_source_enable_remoteplay(void);
+int astro_remote_ps5_protocol_probe(astro_remote_ps5_protocol_probe_t *out);
 
 #endif
