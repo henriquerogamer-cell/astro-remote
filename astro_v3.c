8d242944c1e97cf07bc27892d91c3b883355e60e
henriquerogamer-cell <henriquerogamer@gmail.com>
2026-09-03 21:22:58 -0300

    Install Astro shortcut in PS5 Media

--- astro_v3.c
+++ astro_v3.c
@@ -108,6 +108,7 @@
 #include "astro_v3_homebrew_web.inc"
 #undef curl_easy_perform
 #include "astro_v3_pkg_installer.inc"
+#include "astro_v3_launcher.inc"
 
 /* File manager behaves like a desktop Explorer with a lazy folder tree.
  * The first Explorer cut had one malformed JS statement, so repair the
